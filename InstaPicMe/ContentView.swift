b8c720d40390cc1b097ecdcce2ae9eb76e53499e
Christian Ray Leovido <christian.leovido@gmail.com>
2020-07-28 10:23:53 +0100

    Initial Commit

---
+++ InstaPicMe/ContentView.swift
@@ -0,0 +1,20 @@
+//
+//  ContentView.swift
+//  InstaPicMe
+//
+//  Created by Christian Leovido on 28/07/2020.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!").padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ InstaPicMe/InstaPicMeApp.swift
@@ -0,0 +1,17 @@
+//
+//  InstaPicMeApp.swift
+//  InstaPicMe
+//
+//  Created by Christian Leovido on 28/07/2020.
+//
+
+import SwiftUI
+
+@main
+struct InstaPicMeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
